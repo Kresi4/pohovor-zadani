4566f94142430862316d0a4f033e818796698fb0
Kresi4 <ester.krejzova@gmail.com>
2025-09-09 12:59:36 +0200

    Refactor game board into HerniDeska class
    Introduced HerniDeska class with default ladders and snakes.

--- HadiZebriky/herni_deska.py
+++ HadiZebriky/herni_deska.py
@@ -1,23 +1,29 @@
-ZEBRIKY = {2: 38,
-           7: 14,
-           8: 31,
-           15: 26,
-           21: 42,
-           28: 84,
-           36: 44,
-           51: 67,
-           71: 91,
-           78: 98,
-           87: 94,
-           }
-HADI = {16: 6,
-        46: 25,
-        49: 11,
-        62: 19,
-        64: 60,
-        74: 53,
-        89: 68,
-        92: 88,
-        95: 75,
-        99: 80,
+class HerniDeska:
+    def __init__(self, zebriky=None, hadi=None, velikost=100):
+        # Defaultní žebříky a hadi
+        self.zebriky = zebriky or {
+            2: 38, 7: 14, 8: 31, 15: 26, 21: 42, 28: 84,
+            36: 44, 51: 67, 71: 91, 78: 98, 87: 94
         }
+        self.hadi = hadi or {
+            16: 6, 46: 25, 49: 11, 62: 19, 64: 60, 74: 53,
+            89: 68, 92: 88, 95: 75, 99: 80
+        }
+        self.velikost = velikost
+
+    def aplikuj_specialni_pole(self, pozice, jmeno, vypis_func=print):
+        if pozice in self.zebriky:
+            nova_pozice = self.zebriky[pozice]
+            vypis_func(f"↑ {jmeno} vylezl po žebříku na pole {nova_pozice}")
+            return nova_pozice
+        elif pozice in self.hadi:
+            nova_pozice = self.hadi[pozice]
+            vypis_func(f"🐍 {jmeno} spadl na pole {nova_pozice}")
+            return nova_pozice
+        return pozice
+
+    def je_specialni_pole(self, pozice):
+        return pozice in self.zebriky or pozice in self.hadi
+
+    def je_platne_pole(self, pozice):
+        return 1 <= pozice <= self.velikost
